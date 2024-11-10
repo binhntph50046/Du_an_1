7cd17a89b722a434d712393027807b1226a5dccc
unknown <nguyenmanhcuong17042005@gmail.com>
2024-11-10 20:04:40 +0700

    CRUD Products

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,50 @@
 <?php
-    include 'header.php';
-    include 'sidebar.php';
-    
-    
-    
-    include 'main.php';
+ob_start();
+
+   $act = isset($_GET['act']) ?$_GET['act'] :'/';
+
+   include '../admin/commons/env.php';
+   include '../admin/commons/function.php';
+   include '../admin/models/products.php';
+   include '../admin/controllers/productsController.php';
+   include './views/layout/header.php';
+   include './views/layout/sidebar.php';
+
+
+   $productsController = new ProductsController();
+
+
+   switch($act) {
+      
+      case 'listProducts':
+         $productsController->listProducts();
+         break;
+      case 'formAddProducts':
+         $productsController->formAddProducts();
+         break;
+
+      case 'postFormAdd':
+         $productsController->postFormAdd();
+         break;
+
+      case 'deleteProduct':
+         $productsController->deleteProduct();
+         break;
+      case 'formEditProducts':
+         $productsController->formEditProducts();
+         break;
+      case 'updateProduct':
+         $productsController->updateProduct();
+         break;
+      default:
+         include './views/layout/main.php';
+         break;
+      
+         
+   }
 
 
 
 
-    include 'footer.php';
+
 ?>
