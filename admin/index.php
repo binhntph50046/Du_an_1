646a2c6ca06c12a2154d79b1c3d292366033194d
unknown <nguyenmanhcuong17042005@gmail.com>
2024-11-13 02:39:30 +0700

    ADD Product

--- admin/index.php
+++ admin/index.php
@@ -5,35 +5,59 @@ include '../admin/commons/env.php';
 include '../admin/commons/function.php';
 include '../admin/controllers/CategoryController.php';
 include '../admin/models/Category.php';
-include './views/layout/header.php';
+include '../admin/models/products.php';
+include '../admin/controllers/productsController.php';
+include '../views/layout/header.php';
 
+$productsController = new ProductsController();
 $categoryController = new CategoryController();
 
 switch ($act) {
-    case 'static':
-        include './views/layout/static.php';
-        break;
-    case 'list-category':
-        $categoryController->listCategory();
-        break;
-    case 'form-add-category':
-        $categoryController->formAddCategory();
-        break;
-    case 'post-add-category':
-        $categoryController->postAddCategory();
-        break;
-    case 'form-edit-category':
-        $categoryController->formEditCategory();
-        break;
-    case 'post-edit-category':
-        $categoryController->postEditCategory();
-        break;
-    case 'delete-category':
-        $categoryController->postDeleteCategory();
-        break;
-    default:
-        include './views/layout/main.php';
-        break;
+   case 'static':
+      include './views/layout/static.php';
+      break;
+   case 'list-category':
+      $categoryController->listCategory();
+      break;
+   case 'form-add-category':
+      $categoryController->formAddCategory();
+      break;
+   case 'post-add-category':
+      $categoryController->postAddCategory();
+      break;
+   case 'form-edit-category':
+      $categoryController->formEditCategory();
+      break;
+   case 'post-edit-category':
+      $categoryController->postEditCategory();
+      break;
+   case 'delete-category':
+      $categoryController->postDeleteCategory();
+      break;
+
+   case 'listProducts':
+      $productsController->listProducts();
+      break;
+   case 'formAddProducts':
+      $productsController->formAddProducts();
+      break;
+
+   case 'postFormAdd':
+      $productsController->postFormAdd();
+      break;
+
+   case 'deleteProduct':
+      $productsController->deleteProduct();
+      break;
+   case 'formEditProducts':
+      $productsController->formEditProducts();
+      break;
+   case 'updateProduct':
+      $productsController->updateProduct();
+      break;
+   default:
+      include './views/layout/main.php';
+      break;
 }
 
 include './views/layout/footer.php';
