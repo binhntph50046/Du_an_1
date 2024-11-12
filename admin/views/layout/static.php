204b53c0f98e6567f9cab992f65c3264072259bd
van dai <daicvph50503@gmail.com>
2024-11-13 03:22:26 +0700

    updateList

--- admin/views/layout/static.php
+++ admin/views/layout/static.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .dashboard-container {
-            width: 1170px;
+            width: 1400px;
         }
 
         .card {
