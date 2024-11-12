f3c3164d246a15318005d7e1fdbaf051a116c6b7
van dai <daicvph50503@gmail.com>
2024-11-13 03:38:21 +0700

    daideptrainhatquadat

--- admin/views/layout/static.php
+++ admin/views/layout/static.php
@@ -9,10 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .dashboard-container {
-<<<<<<< HEAD
-            width: 1250px;
-=======
->>>>>>> b8a5d73962ac88c939774b1a5f2dfabb88a626f9
+            min-width: 80%;
+            margin: 0 auto;
         }
         .card {
             margin-bottom: 20px;
