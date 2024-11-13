ecff69ae7bb18c76c4dcad4b0549ad2e4fe6961a
Chuvandai <166127597+Chuvandai@users.noreply.github.com>
2024-11-13 13:20:34 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -55,8 +55,44 @@ switch ($act) {
    case 'updateProduct':
       $productsController->updateProduct();
       break;
+   case 'listUser':
+        $userController->listUser();
+        break;
+    case 'create':
+        $userController->create();
+        break;
+    case 'updateUser':
+        $userController->edit($_GET['id']);
+        break;
+    case 'delete':
+        $userController->delete($_GET['id']);
+        break;
+    case 'comments':
+        $commentController->index();
+        include "views/comments/listComment.php";
+        break;
+    case 'approveComment':
+        $commentController->approve();
+        break;
+    case 'rejectComment':
+        $commentController->reject();
+        break;
+    case 'deleteComment':
+        $commentController->delete();
+        break;
+    case 'orders':
+        $orderController->index();
+        break;
+    case 'viewOrder':
+        $orderController->view($_GET['id']);
+        break;
+    case 'updateOrderStatus':
+        $orderController->updateStatus();
+        break;
+    case 'deleteOrder':
+        $orderController->delete($_GET['id']);
+        break;
    default:
-      
       break;
 }
 
