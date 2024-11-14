9527da887749635fc91d457788e7f2aee73fbcb2
van dai <daicvph50503@gmail.com>
2024-11-14 18:25:49 +0700

    cilent

--- client/views/header.php
+++ client/views/header.php
@@ -28,7 +28,7 @@
                 </div>
                 <div class="menu-button"><i class="fa-solid fa-list"></i> Danh mục</div>
                 <div class="content1">
-                    <input type="text" class="search-bar" placeholder="Nhập tên điện thoại, máy tính, phụ kiện... cần tìm">
+                    <input type="text" class="search-bar" placeholder="Hôm nay bạn cần gì ?">
                     <button class="search-button"><i class="fa-solid fa-magnifying-glass"></i></button>
                 </div>
 
