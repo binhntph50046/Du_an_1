e1bc79c7f1c1e44278b9a73f75f51a776ede202d
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2024-11-20 02:18:10 +0700

    update

--- admin/views/Products/formAddProducts.php
+++ admin/views/Products/formAddProducts.php
@@ -39,15 +39,14 @@
 
                         <div class="mb-3">
                             <label for="productName" class="form-label">Ngày Nhập</label>
-                            <input type="date" class="form-control" id="productName"
-                                name="ngay_nhap" required>
+                            <input type="date" class="form-control" id="productName" style="width: 150px;" name="ngay_nhap" style="width: 150px;" required>
                         </div>
                         <div class="mb-3">
                             <label for="categoryDescription" class="form-label">Mô Tả</label>
                             <textarea class="form-control" id="categoryDescription" name="mo_ta"
                                 rows="3" required></textarea>
                         </div>
-                       
+
                         <div class="mb-3">
                             <label for="productName" class="form-label">Hình Ảnh</label>
                             <input type="file" class="form-control" id="productName"
@@ -59,7 +58,7 @@
                                 required>
                                 <option value="2">Trạng thái</option>
                                 <option value="1">Còn</option>
-                                
+
                                 <option value="0">Hết</option>
                             </select>
                         </div>
