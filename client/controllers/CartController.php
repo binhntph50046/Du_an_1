9047f617e9d0ded82bda4e88ace94ab503526c15
binhntph50046 <binhntph50046@gmail.com>
2024-11-23 00:09:18 +0700

    fix gio hang

--- client/controllers/CartController.php
+++ client/controllers/CartController.php
@@ -64,7 +64,7 @@ class CartController {
         }
         
         $cartItems = $this->cartModel->getCartItems($_SESSION['email']['tai_khoan_id']);
-        include 'views/cart.php';
+        include 'views/cart/cart.php';
     }
 
     public function updateCart() {
